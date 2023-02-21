de18fae6e0e10f59423ab4eddff22362b07f83ec
wd210010 <76995206+wd210010@users.noreply.github.com>
2023-02-21 16:41:21 +0800

    一点万向签到领万向星 可抵扣停车费

--- ydwx.py
+++ ydwx.py
@@ -1,5 +1,6 @@
 import requests,time,hashlib,json
 
+#一点万向签到领万向星 可抵扣停车费
 #登录后搜索https://app.mixcapp.com/mixc/gateway域名随意一个 请求体里面的deviceParams，token 多账号填多个单引号里面 用英文逗号隔开(现在的格式是2个账号的 如果单账号 改成deviceParams=['deviceParams1'])
 deviceParams=['deviceParams1','deviceParams2']
 token =['token1','token2']
@@ -32,12 +33,14 @@ def Push(contents):
 
 
 print(f'共配置了{len(deviceParams)}个账号')
+log = []
 for i in range(len(deviceParams)):
   print(f'*****第{str(i+1)}个账号*****')
   timestamp = str(int(round(time.time() * 1000)))
   md5 = hashlib.md5()
+  md52 = hashlib.md5()
   sig = f'action=mixc.app.memberSign.sign&apiVersion=1.0&appId=68a91a5bac6a4f3e91bf4b42856785c6&appVersion=3.32.0&deviceParams={deviceParams[i]}&imei=2333&mallNo=20014&osVersion=12.0.1&params=eyJtYWxsTm8iOiIyMDAxNCJ9&platform=h5&timestamp={timestamp}&token={token[i]}&P@Gkbu0shTNHjhM!7F' # 创建md5加密对象
-  md5.update(sig.encode('utf-8'))  	# 指定需要加密的字符串
+  md5.update(sig.encode('utf-8'))  # 指定需要加密的字符串
   sign = md5.hexdigest()  			# 加密后的字符串
   url = 'https://app.mixcapp.com/mixc/gateway'
   headers=  {
@@ -57,6 +60,8 @@ for i in range(len(deviceParams)):
   }
   data =f'mallNo=20014&appId=68a91a5bac6a4f3e91bf4b42856785c6&platform=h5&imei=2333&appVersion=3.32.0&osVersion=12.0.1&action=mixc.app.memberSign.sign&apiVersion=1.0&timestamp={timestamp}&deviceParams={deviceParams[i]}&token={token[i]}&params=eyJtYWxsTm8iOiIyMDAxNCJ9&sign={sign}'
   html = requests.post(url=url,headers=headers,data=data)
-  result = json.loads(html.text)['message']
+  result = f'帐号{i+1}签到结果:'+'' +json.loads(html.text)['message']
   print(result)
-  Push(contents=result)
+  log.append(result)
+log2 = str(log).replace('[\'','').replace('\']','').replace(':','\n').replace('\', \'','\n')
+Push(contents=log2)
