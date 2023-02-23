dc2ccf814210b7a931312cdfb05cd5c8732ee4f2
wd210010 <76995206+wd210010@users.noreply.github.com>
2023-02-23 15:14:54 +0800

    #一点万向签到领万向星 可抵扣停车费

--- ydwx.py
+++ ydwx.py
@@ -1,30 +1,21 @@
 import requests,time,hashlib,json
+import os
 
 #一点万向签到领万向星 可抵扣停车费
-#登录后搜索https://app.mixcapp.com/mixc/gateway域名随意一个 请求体里面的deviceParams，token 多账号填多个单引号里面 用英文逗号隔开(现在的格式是2个账号的 如果单账号 改成deviceParams=['deviceParams1'])
-deviceParams=['deviceParams1','deviceParams2']
-token =['token1','token2']
+#登录后搜索https://app.mixcapp.com/mixc/gateway域名随意一个 请求体里面的deviceParams，token 多账号填多个单引号里面 用英文逗号隔开
+# 本地运行用取消下面两行注释 并注释掉青龙变量的两行变量
+# ydwx_deviceParams=['deviceParams1','deviceParams2']
+# ydwx_token =['token1','token2']
 
-# 企业微信推送参数
-corpid = ''
-agentid = ''
-corpsecret = ''
-touser = ''
-# 推送加 token
-plustoken = ''
+# 青龙变量
+ydwx_deviceParams = os.getenv("ydwx_deviceParams").split('&')
+ydwx_token = os.getenv("ydwx_token").split('&')
+
+#推送加 token
+plustoken = os.getenv("plustoken")
 
 def Push(contents):
-  # 微信推送
-  if all([corpid, agentid, corpsecret, touser]):
-    token = \
-      requests.get(
-        f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}').json()[
-        'access_token']
-    json = {"touser": touser, "msgtype": "text", "agentid": agentid, "text": {"content": "一点万向签到\n" + contents}}
-    resp = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={token}", json=json)
-    print('微信推送成功' if resp.json()['errmsg'] == 'ok' else '微信推送失败')
-
-  if plustoken:
+  # plustoken推送
     headers = {'Content-Type': 'application/json'}
     json = {"token": plustoken, 'title': '一点万向签到', 'content': contents.replace('\n', '<br>'), "template": "json"}
     resp = requests.post(f'http://www.pushplus.plus/send', json=json, headers=headers).json()
@@ -32,14 +23,14 @@ def Push(contents):
 
 
 
-print(f'共配置了{len(deviceParams)}个账号')
+print(f'共配置了{len(ydwx_deviceParams)}个账号')
 log = []
-for i in range(len(deviceParams)):
+for i in range(len(ydwx_deviceParams)):
   print(f'*****第{str(i+1)}个账号*****')
   timestamp = str(int(round(time.time() * 1000)))
   md5 = hashlib.md5()
   md52 = hashlib.md5()
-  sig = f'action=mixc.app.memberSign.sign&apiVersion=1.0&appId=68a91a5bac6a4f3e91bf4b42856785c6&appVersion=3.32.0&deviceParams={deviceParams[i]}&imei=2333&mallNo=20014&osVersion=12.0.1&params=eyJtYWxsTm8iOiIyMDAxNCJ9&platform=h5&timestamp={timestamp}&token={token[i]}&P@Gkbu0shTNHjhM!7F' # 创建md5加密对象
+  sig = f'action=mixc.app.memberSign.sign&apiVersion=1.0&appId=68a91a5bac6a4f3e91bf4b42856785c6&appVersion=3.32.0&deviceParams={ydwx_deviceParams[i]}&imei=2333&mallNo=20014&osVersion=12.0.1&params=eyJtYWxsTm8iOiIyMDAxNCJ9&platform=h5&timestamp={timestamp}&token={ydwx_token[i]}&P@Gkbu0shTNHjhM!7F' # 创建md5加密对象
   md5.update(sig.encode('utf-8'))  # 指定需要加密的字符串
   sign = md5.hexdigest()  			# 加密后的字符串
   url = 'https://app.mixcapp.com/mixc/gateway'
@@ -58,10 +49,11 @@ for i in range(len(deviceParams)):
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
   }
-  data =f'mallNo=20014&appId=68a91a5bac6a4f3e91bf4b42856785c6&platform=h5&imei=2333&appVersion=3.32.0&osVersion=12.0.1&action=mixc.app.memberSign.sign&apiVersion=1.0&timestamp={timestamp}&deviceParams={deviceParams[i]}&token={token[i]}&params=eyJtYWxsTm8iOiIyMDAxNCJ9&sign={sign}'
+  data =f'mallNo=20014&appId=68a91a5bac6a4f3e91bf4b42856785c6&platform=h5&imei=2333&appVersion=3.32.0&osVersion=12.0.1&action=mixc.app.memberSign.sign&apiVersion=1.0&timestamp={timestamp}&deviceParams={ydwx_deviceParams[i]}&token={ydwx_token[i]}&params=eyJtYWxsTm8iOiIyMDAxNCJ9&sign={sign}'
   html = requests.post(url=url,headers=headers,data=data)
   result = f'帐号{i+1}签到结果:'+'' +json.loads(html.text)['message']
   print(result)
   log.append(result)
 log2 = str(log).replace('[\'','').replace('\']','').replace(':','\n').replace('\', \'','\n')
+# print(log2)
 Push(contents=log2)
