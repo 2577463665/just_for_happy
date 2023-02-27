dbc502bded6915d3577d394be80bbf09289c2e58
wd210010 <76995206+wd210010@users.noreply.github.com>
2023-02-27 08:18:11 +0800

    科技玩家签到

--- kjwj.py
+++ kjwj.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+# -- coding: utf-8 --
+# -------------------------------
+# @Author : github@wd210010 https://github.com/wd210010/just_for_happy
+# @Time : 2023/2/27 13:23
+# -------------------------------
+# cron "30 7 * * *" script-path=xxx.py,tag=匹配cron用
+# const $ = new Env('科技玩家签到')
+
 import requests, json, re,os
 
 
